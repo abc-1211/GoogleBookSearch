13d9fc8b7e303adeaf48b5aec6662cf912b50da3
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 13:08:24 +0800

    search page included nav, headbox, search box

--- booksearch/src/pages/search.js
+++ booksearch/src/pages/search.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../component/navbar";
+import SearchBox from "../component/searchbox";
+import HeadBox from "../component/headbox";
 
 
 function Search() {
     return (
         <React.Fragment>
             <Navbar/>
+            <HeadBox />
+            <SearchBox />
         </React.Fragment>
     )
 };
