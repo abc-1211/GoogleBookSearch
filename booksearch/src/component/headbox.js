f8aed930dad14979ea4c3bae241591937db3640b
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 13:07:57 +0800

    headbox complete

---
+++ booksearch/src/component/headbox.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HeadBox() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="card center">
+          <h1>(React) Google Books Search</h1>
+          <h5>Search for and Save Books of Interest</h5>
+          <br/>
+          <br/>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default HeadBox;
