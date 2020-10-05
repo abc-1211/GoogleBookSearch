2ccd6e54994c2362d5dc131c69d986dffafa19f0
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 17:27:12 +0800

    saved page complete

--- booksearch/src/pages/saved.js
+++ booksearch/src/pages/saved.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../component/navbar";
-
+import HeadBox from "../component/headbox";
+import SavedBox from "../component/savedbox";
 
 function Saved() {
     return (
         <React.Fragment>
             <Navbar/>
+            <HeadBox />
+            <SavedBox />
         </React.Fragment>
     )
 };
