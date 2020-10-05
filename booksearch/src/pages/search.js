6c14171530c98bb98dd376a8bb7b8b46fcdb2fd7
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 13:26:11 +0800

    search page UI completed, next step fill in form and eventlistener

--- booksearch/src/pages/search.js
+++ booksearch/src/pages/search.js
@@ -2,14 +2,18 @@ import React from "react";
 import Navbar from "../component/navbar";
 import SearchBox from "../component/searchbox";
 import HeadBox from "../component/headbox";
+import ResultBox from "../component/resultbox";
 
 
 function Search() {
+    const books = [{bookName:"test"},{bookName:"test"}];
+
     return (
         <React.Fragment>
             <Navbar/>
             <HeadBox />
             <SearchBox />
+            <ResultBox books={books}/>
         </React.Fragment>
     )
 };
