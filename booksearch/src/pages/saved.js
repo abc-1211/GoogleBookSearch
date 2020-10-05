e33518b3d721e3825d07c6d888490853a5c06166
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 12:50:25 +0800

    Pages initialize

---
+++ booksearch/src/pages/saved.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../component/navbar";
+
+
+function Saved() {
+    return (
+        <React.Fragment>
+            <Navbar/>
+        </React.Fragment>
+    )
+};
+
+export default Saved;
---
+++ booksearch/src/pages/search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../component/navbar";
+
+
+function Search() {
+    return (
+        <React.Fragment>
+            <Navbar/>
+        </React.Fragment>
+    )
+};
+
+export default Search;
