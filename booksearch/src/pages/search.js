9dfd1bd8d90ff5d88cc72f7a25f1d4ef6c0eedea
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 13:32:09 +0800

    useForm, handleSubmit

--- booksearch/src/pages/search.js
+++ booksearch/src/pages/search.js
@@ -3,16 +3,21 @@ import Navbar from "../component/navbar";
 import SearchBox from "../component/searchbox";
 import HeadBox from "../component/headbox";
 import ResultBox from "../component/resultbox";
-
+import { useForm } from "react-hook-form";
 
 function Search() {
+    const { register, handleSubmit } = useForm();
     const books = [{bookName:"test"},{bookName:"test"}];
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
     return (
         <React.Fragment>
             <Navbar/>
             <HeadBox />
-            <SearchBox />
+            <SearchBox handleSubmit={handleSubmit(onSubmit)} register={register} />
             <ResultBox books={books}/>
         </React.Fragment>
     )
