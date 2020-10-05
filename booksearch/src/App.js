57726ed1a55b72af881d9807718a4b2376ea93b6
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 12:50:17 +0800

    Routes added

--- booksearch/src/App.js
+++ booksearch/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
-import Navbar from "./component/navbar";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "./pages/search";
+import Saved from "./pages/saved";
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
