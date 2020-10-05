13ad1b18993f6e2db12c036443c3ce7111a29eb0
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 17:27:25 +0800

    search page completed

--- booksearch/src/pages/search.js
+++ booksearch/src/pages/search.js
@@ -6,31 +6,46 @@ import ResultBox from "../component/resultbox";
 import { useForm } from "react-hook-form";
 import services from "../services";
 
-
 function Search() {
-    const { register, handleSubmit } = useForm();
-    const [books, setBooks] = useState([]);
+  const { register, handleSubmit } = useForm();
+  const [books, setBooks] = useState([]);
 
-    const onSubmit = async (data) => {
-        try {
-            const api = await services.searchBook(data.searchWord);
-            console.log(api);
-            setBooks(api.items);
-        }
-        catch (err) {
-            return err;
-        }
-        
-    };
+  const onSubmit = async (data) => {
+    try {
+      const api = await services.searchBook(data.searchWord);
+      console.log(api);
+      setBooks(api.items);
+    } catch (err) {
+      return err;
+    }
+  };
 
-    return (
-        <React.Fragment>
-            <Navbar/>
-            <HeadBox />
-            <SearchBox handleSubmit={handleSubmit(onSubmit)} register={register} />
-            <ResultBox books={books}/>
-        </React.Fragment>
-    )
-};
+  const save = async (index) => {
+    try {
+      let { title, infoLink, authors, description, imageLinks } = books[index].volumeInfo;
+      let data = {
+        title,
+        infoLink,
+        authors,
+        description,
+        imageLinks,
+      };
+      console.log(data);
+      const res = await services.setSaved(data);
+      console.log(res.msgBody);
+    } catch (err) {
+      return err;
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Navbar />
+      <HeadBox />
+      <SearchBox handleSubmit={handleSubmit(onSubmit)} register={register} />
+      <ResultBox books={books} save={save} />
+    </React.Fragment>
+  );
+}
 
 export default Search;
