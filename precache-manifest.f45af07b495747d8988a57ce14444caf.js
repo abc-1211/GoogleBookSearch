120c00f0999491b833e9cfd4b8e4c2c57305ec9c
ian <chengkwanyu@yahoo.com.hk>
2020-10-07 12:51:33 +0800

    Updates

---
+++ precache-manifest.f45af07b495747d8988a57ce14444caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37acd85aecce0fb448b4e23c0c128fd",
+    "url": "/GoogleBookSearch/index.html"
+  },
+  {
+    "revision": "11d2a28fcbc488c738db",
+    "url": "/GoogleBookSearch/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "1379555b4fa77f25d132",
+    "url": "/GoogleBookSearch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11d2a28fcbc488c738db",
+    "url": "/GoogleBookSearch/static/js/2.52a3df9e.chunk.js"
+  },
+  {
+    "revision": "bd2dbd39ae7ff0986c133c4c2c8c7f82",
+    "url": "/GoogleBookSearch/static/js/2.52a3df9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1379555b4fa77f25d132",
+    "url": "/GoogleBookSearch/static/js/main.87b0f270.chunk.js"
+  },
+  {
+    "revision": "a7e4c7a5b4bd64c2520d",
+    "url": "/GoogleBookSearch/static/js/runtime-main.2c291538.js"
+  }
+]);
